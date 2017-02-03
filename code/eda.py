4c63f8048f2c835713f00d241fecf4e9fa7ad37e
RodrigoGonzalez <rodrigo.gonzalez@colorado.edu>
2017-02-02 19:50:58 -0700

    docker container usage instructions

--- code/eda.py
+++ code/eda.py
@@ -2,4 +2,9 @@ import numpy as np
 import pandas as pd
 import h5py
 
+# docker run -it kagglegym
+# python
+# >>> import kagglegym
+# >>> kagglegym.test()
+
 train = pd.read_hdf("../data/train.h5")
